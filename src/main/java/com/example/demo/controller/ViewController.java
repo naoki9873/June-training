f25f7e2ca5aeaa3a26659b259cb2dc7061377590
naoki <naoki.naoki.417.417.417@gmail.com>
2026-06-07 15:26:00 +0900

    search

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +17,33 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class ViewController {
-	
+
 	private final UserService userService;
-	
+	private static final int PAGE_SIZE = 10;
+
 	//一覧表示
 	@GetMapping("/view")
-	public String view(@RequestParam(defaultValue = "0") int page , Model model) {
-		
-		Page<UserEntity> userPage = userService.findAll(page);
-		
-		
-		
+	public String view(
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(required = false) Integer id,
+			Model model) {
+		//インスタンス生成
+		Pageable pageable = PageRequest.of(page, PAGE_SIZE, Sort.by("id").ascending());
+		Page<UserEntity> userPage;
+
+		//検索処理
+		if (id != null) {
+			//id検索
+			userPage = userService.findById(id, pageable);
+		} else {
+			//全件取得
+			userPage = userService.findAll(pageable);
+		}
+
 		//HTMLに渡す　「userList」これ大事
-		model.addAttribute("userPage" , userPage);
-		
+		model.addAttribute("userPage", userPage);
 		//HTML返す
 		return "view";
 	}
-	
+
 }
